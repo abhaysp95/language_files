138f65bbe3d0f9bfddca0a4ae84cd586189b17b1
coolabhays <abhaysp9955@gmail.com>
2020-02-04 22:53:19 +0530

    improved

--- nc/sec_largest.c
+++ nc/sec_largest.c
@@ -4,7 +4,7 @@
 #define size 15
 
 int main(int argc, char *argv[]){
-	int n,i,max,sec,a[size];
+	int n,i,max,sec,a[size],j;
 	printf("\nEnter the number of elements to enter in array: ");
 	scanf("%d",&n);
 	printf("\nEnter the elements in the array: ");
@@ -12,14 +12,25 @@ int main(int argc, char *argv[]){
 		printf("\n%d:",i+1);
 		scanf("%d",&a[i]);
 	}
-	max = a[0];
+	sec = max = a[0];
 	for (i = 1; i < n; ++i) {
 		if (a[0] < a[i]) {
 				sec = max;
 				max = a[i];
+				j = i;
+			}
+		else if (sec < a[i] && max != a[i]) {
+				sec = a[i];
+				/* c = i; */
+		}
+	}
+	for (i = 0; i < n; ++i) {
+		if (a[i] == sec ) {
+			break;
 			}
 	}
-	printf("\nLargest element is %d in position %d. \n Second largest element is %d",max,i,sec);
+	printf("\nPosition printed according to index number.");
+	printf("\nLargest element is %d in position %d. \nSecond largest element is %d in %d position",max,j,sec,i);
 	return 0;
 }
 
