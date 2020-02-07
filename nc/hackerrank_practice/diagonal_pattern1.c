d8f5660dde7bd0dc5958472e8fdfa2f962e822b4
coolabhays <abhaysp9955@gmail.com>
2020-02-07 23:48:16 +0530

    square pattern with spaces

---
+++ nc/hackerrank_practice/diagonal_pattern1.c
@@ -0,0 +1,55 @@
+/*** Diagonal pattern printing program ***/
+
+#include<stdio.h>
+
+
+int main(int argc, char *argv[]){
+	int n,i,j,k,l;
+	printf("\nEnter the number: ");
+	scanf("%d",&n);
+	// for the upper part
+	for (i = 0; i < n; i++) {
+		// for the first triangle
+		for (j = 0; j < i; j++) {
+			printf("%d",n-j);
+			/* if (j < i-1) */
+				printf(" ");
+		}
+		// for the mid triangle
+		for (k = 0; k < (2*(n-i))-1; k++) {
+			printf("%d",n-i);
+			/* if (k <) */
+			printf(" ");
+		}
+		// for the last triangle
+		for (l = 0; l < i; l++) {
+			printf("%d",n-i+l+1);
+			printf(" ");
+		}
+		printf("\n");
+	}
+
+	// for the bottom part
+	for (i = 1; i < n; i++) {
+		// for the first triangle
+		for (j = 0; j < n-i; j++) {
+			printf("%d",n-j);
+			printf(" ");
+		}
+		// for the mid triangle
+		for (k = 0; k < (2*i)-1; k++) {
+			printf("%d",i+1);
+			printf(" ");
+		}
+		// for the last triangle
+		for (l = 0; l < n-i; l++) {
+			printf("%d",n-j+l+1);
+			printf(" ");
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
+
+
