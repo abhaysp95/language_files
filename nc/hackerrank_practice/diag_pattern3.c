d002804bb72c7df36c494cd9883838612af4edbf
coolabhays <abhaysp9955@gmail.com>
2020-02-07 23:46:47 +0530

    hour glass pattern

--- nc/hackerrank_practice/diag_pattern3.c
+++ nc/hackerrank_practice/diag_pattern3.c
@@ -9,14 +9,29 @@ Last Edit -> 2020-02-07 */
 
 
 int main(int argc, char *argv[]){
-	int n,i,j,k;
+	int n,i,j,k,l;
 	printf("Enter the number: ");
 	scanf("%d",&n);
-	for (i = n; i >=0 ; i--) {
-		for (j = 0; j < i; ++j) {
+	for (i = n; i >0 ; i--) {
+		for (j = 0; j < n-i; ++j) {
 				printf(" ");
 		}
-		for (k = 0 ;k < n-i; k++) {
+		for (k = 0 ;k < i; k++) {
+			printf("*");
+		}
+		for (l = 0; l < i-1; l++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	for (i = n-1; i > 0; i--) {
+		for (j = 0; j < i-1; j++) {
+			printf(" ");
+		}
+		for (k = 0; k <= n-i; k++) {
+			printf("*");
+		}
+		for (l = 0; l < n-i; l++) {
 			printf("*");
 		}
 		printf("\n");
