98d78009dceea1f53c31070bc5e5a8aa5570c545
coolabhays <abhaysp9955@gmail.com>
2020-02-11 07:34:44 +0530

    modified

--- nc/hackerrank_practice/bitwise-operators-in-c-English.c
+++ nc/hackerrank_practice/bitwise-operators-in-c-English.c
@@ -10,7 +10,7 @@ Last Edit -> 2020-02-11 */
 #define size 50
 
 void calculate_the_maximum(int ,int );
-void sort_array(int arr[], int );
+void sort_array(int *arr3, int );
 
 int main(int argc, char *argv[]){
 	int n, k;
@@ -34,6 +34,19 @@ void calculate_the_maximum(int n, int k) {
 			l++; p++; q++;
 		}
 	}
+	printf("\n\n");
+	for (i = 0; i < l; i++) {
+		printf("%d ",arr[i]);
+	}
+	printf("\n\n");
+	for (i = 0; i < p; i++) {
+		printf("%d ",arr1[i]);
+	}
+	printf("\n\n");
+	for (i = 0; i < q; i++) {
+		printf("%d ",arr2[i]);
+	}
+	printf("\n\n");
 	sort_array(arr, l);
 	sort_array(arr1, p);
 	sort_array(arr2, q);
@@ -53,14 +66,14 @@ void calculate_the_maximum(int n, int k) {
 
 }
 
-void sort_array(int arr[], int l) {
+void sort_array(int *arr3, int l) {
 	int i, j, temp;
 	for (i = 0; i < l; ++i) {
 		for (j = 0; j < l-i; j++) {
-			if (arr[j+1] < arr[j]) {
-				temp = arr[j+1];
-				arr[j + 1] = arr[j];
-				arr[j] = temp;
+			if (arr3[j+1] < arr3[j]) {
+				temp = arr3[j+1];
+				arr3[j + 1] = arr3[j];
+				arr3[j] = temp;
 			}
 		}
 	}
