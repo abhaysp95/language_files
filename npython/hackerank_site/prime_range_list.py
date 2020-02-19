0d993dfc512cf11072a40904a14aaa174e96742c
coolabhays <abhaysp9955@gmail.com>
2020-02-19 13:19:12 +0530

    new

--- npython/hackerank_site/prime_range_list.py
+++ npython/hackerank_site/prime_range_list.py
@@ -14,3 +14,18 @@ n = int(input())
 for i in range(n):
     num = int(input())
     arr.append(num)
+c = 0
+for i in range(n):
+    flag = 0
+    if (arr[i] == 2):
+        flag = 0
+    elif (arr[i] == 1):
+        flag = 1
+    else:
+        for j in range(2, arr[i]):
+            if (arr[i] % j == 0):
+                flag = 1
+                break
+        if flag == 0:
+            c += 1
+print(c)
