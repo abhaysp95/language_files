df983dc3b881d3f5591bc86631a2dae6fcdcbc5b
coolabhays <abhaysp9955@gmail.com>
2020-02-20 07:47:22 +0530

    new question

---
+++ nc/hackerrank_practice/remove_friend(singlell_deletion).c
@@ -0,0 +1,105 @@
+/** Name -> Abhay Shanker Pathak
+Date -> 2020-02-20
+Description -> Remove maximux element from linked list
+Email -> abhaysp9955@gmail.com
+Github -> https://github.com/CoolAbhayS
+Last Edit -> 2020-02-20 */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+	int data;
+	struct node *link;
+} *root = NULL;
+
+struct node *create_ll(struct node *);
+struct node *display(struct node *);
+struct node *max_del(struct node *);
+
+int main(int argc, char *argv[]){
+	// creation of linked list
+	root = create_ll(root);
+	// finding max element and deleting it
+	root = max_del(root);
+	// display list
+	root = display(root);
+	return 0;
+}
+
+struct node *create_ll(struct node *root) {
+	struct node *ptr, *temp;
+	int val, num, arr[50];
+	printf("\nEnter the limit: ");
+	scanf("%d", &val);
+	if (val >= 1 && val <= 10) {
+		while (val > 0) {
+			printf("\nEnter the element: ");
+			scanf("%d", &num);
+			temp = (struct node *)malloc(sizeof(struct node));
+			temp -> data = num;
+			temp -> link = NULL;
+			if (root == NULL) {
+				root = temp;
+			}
+			else {
+				ptr = root;
+				while (ptr -> link != NULL) {
+					ptr = ptr -> link;
+				}
+				ptr -> link = temp;
+				temp -> link = NULL;
+			}
+			val--;
+		}
+	}
+	else {
+		while (val > 0) {
+			int i = 0;
+			scanf("%d", &arr[i]);
+			val--;
+			i++;
+		}
+		printf("\nfriend list exceeded from limit");
+	}
+	return root;
+}
+
+struct node *max_del(struct node *root) {
+	int max = 0;
+	struct node *ptr, *preptr;
+	ptr = root;
+	max = ptr -> data;
+	while (ptr -> link != NULL) {
+		ptr = ptr -> link;
+		if (max < ptr -> data) {
+			max = ptr -> data;
+		}
+	}
+	printf("\nMax element is %d", max);
+	ptr = root;
+	if (ptr -> data == max) {
+		root = root -> link;
+		free(ptr);
+	}
+	else {
+		while (ptr -> data != max) {
+			preptr = ptr;
+			ptr = ptr -> link;
+		}
+		preptr -> link = ptr -> link;
+		free(ptr);
+	}
+	return root;
+}
+
+struct node *display(struct node *root) {
+	struct node *ptr;
+	ptr = root;
+	printf("\nList is: ");
+	while (ptr != NULL) {
+		printf("\n%d", ptr -> data);
+		ptr = ptr -> link;
+	}
+	return root;
+}
