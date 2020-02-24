7946074093cb801f9608f8524747e643e0570bdd
coolabhays <abhaysp9955@gmail.com>
2020-02-24 15:27:59 +0530

    somethings

--- npython/hackerank_site/nested_lists.py
+++ npython/hackerank_site/nested_lists.py
@@ -12,9 +12,20 @@ Last Edited: 2020-02-23
 if __name__ == "__main__":
     #n = int(input())
     lst = [[input(), float(input())] for _ in range(int(input()))]
-    #lst.sort()
+    #print(lst)
+    lst.sort()
     # lst1 = []
     # lst1 = sorted(lst)[1]
     # print(lst)
-    sec_highest = sorted(list(set([marks for name, marks in lst])))[1]
-    print('\n'.join([a for a, b in sorted(lst) if b == sec_highest]))
+    # sec_highest = sorted(list(set([marks for name, marks in lst])))[1]
+    # print('\n'.join([a for a, b in sorted(lst) if b == sec_highest]))
+    sec_lowest = sorted(lst, key = lambda x:x[1])
+    print(type(sec_lowest))
+    lst1 = []
+    for i in range(1, len(sec_lowest) - 1):
+        lst1.append(sec_lowest[1])
+        if sec_lowest[1] != sec_lowest[i + 1]:
+            break
+    # print(*lst1, sep='\n')
+    #print(lst1)
+    print(*lst1, sep=' ')
