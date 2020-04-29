aa558354a258b2385b07bfc927798c630f0950f4
coolabhays <abhaysp9955@gmail.com>
2020-04-29 09:38:31 +0530

    new OOP basic program(complete)

---
+++ npython/ictiitk_python/lecture19/user_class.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+"""
+Author: Abhay Shanker Pathak
+Date: 2020-04-29
+Description: User Class storing and getting details of user
+Email: abhaysp9955@gmail.com
+Last Edit: 2020-04-29
+"""
+
+from datetime import datetime
+
+
+# user class <<<
+class User:
+    '''Specifying information about user'''
+    def __init__(self, user_name, home_dir, login_time, logout_itme):
+        '''creating method for various information on user'''
+        self.hostname = "server"
+        self.name = user_name
+        self.home_folder = home_dir
+        self.login = login_time
+        self.logout = logout_itme
+
+    def describe_user(self):
+        '''Giving details of user'''
+        print(f"User's name is {(self.name).title()}.")
+        if self.home_folder != 'no':
+            print(f"User's home folder exist at /home/{self.name}")
+        print("Login time of user is ", self.login)
+        print("Logout time of user is ", self.logout)
+
+    def greet_user(self):
+        '''Greeting user according to the system time'''
+        current_hour = datetime.today().time().hour
+        if 0 <= current_hour <= 12:
+            print("Hello, ", self.name, " Very Good Moring")
+        elif 12 < current_hour <= 4:
+            print("Hello, ", self.name, " Good Afternoon")
+        else:
+            print("Hello ", self.name, " Good Evening")
+# >>>
+
+
+# main class <<<
+def main():
+    '''Entering in main class'''
+    first_user = User('abhay', 'yes', '12.00', '16.00')
+    second_user = User('thor', 'no', '2:00', '5:00')
+
+    first_user.greet_user()
+    first_user.describe_user()
+
+    print()
+
+    second_user.greet_user()
+    second_user.describe_user()
+# >>>
+
+
+if __name__ == "__main__":
+    main()
