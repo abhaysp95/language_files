cbfa1135d1c3cbfedab4c9315ba1d8836107a5d9
coolabhays <abhaysp9955@gmail.com>
2020-05-02 21:21:43 +0530

    done fibonacci

---
+++ npython/hackerank_site/2020-05_ques/tcsnqt_fibonacci.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+"""
+Author: Abhay Shanker Pathak
+Date: 2020-05-02
+Description: fibonacci series
+Email: abhaysp9955@gmail.com
+Last Edit: 2020-05-02
+"""
+
+
+PROMPT = '> '
+
+
+# function to get fib <<<
+def fib(number):
+    '''function will return integer from fibonacci series for given position'''
+    if number == 0:
+        return 0
+    if number <= 2:
+        return 1
+    return fib(number - 1) + fib(number - 2)
+# >>>
+
+
+# main function <<<
+def main():
+    '''main function providing range to fib()'''
+    num = int(input(PROMPT).strip())
+
+    result = fib(num)
+
+    print("Fibonacci number on ", num, " is: ", result)
+# >>>
+
+
+if __name__ == "__main__":
+    main()
