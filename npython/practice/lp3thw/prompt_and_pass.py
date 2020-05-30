80df6a1a9d378dbfcc1d472b8ec3c8bd7240913a
coolabhays <abhaysp9955@gmail.com>
2020-05-30 11:39:49 +0530

    test repr

--- npython/practice/lp3thw/prompt_and_pass.py
+++ npython/practice/lp3thw/prompt_and_pass.py
@@ -32,6 +32,13 @@ def main():
     print(f"What kind of computer do you have?")
     computer = input(prompt)
 
+    # using repr puts multiline into single line
+    # print(repr(f'''
+    # Alright, so you said '{likes}' about liking me.
+    # You live in {lives}. Not sure where that is.
+    # And you have a {computer} computer. Nice.
+    # '''))
+
     print(f'''
     Alright, so you said '{likes}' about liking me.
     You live in {lives}. Not sure where that is.
