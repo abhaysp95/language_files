7140f0054980e95462c5c5ce44084008815fc7f9
coolabhays <abhaysp9955@gmail.com>
2020-06-18 22:43:07 +0530

    started new

---
+++ nc/practice/hacking_TAE/loop.c
@@ -0,0 +1,20 @@
+/**
+
+Abhay Shanker Pathak
+abhaysp9955@gmail.com
+https://github.com/coolabhays
+
+
+basic loop program to understand assembly language and register */
+
+
+#include<stdio.h>
+
+// main function
+int main(int argc, char* argv[]) {
+	int i;
+	for (i = 0; i < 10; i++) {
+		printf("Hello, World\n");
+	}
+	return 0;
+}
