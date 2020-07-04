e8995a3a7e80e0dbf08ad8c101559a2f61eb2246
coolabhays <abhaysp9955@gmail.com>
2020-07-04 21:49:51 +0530

    removed

--- npython/practice/sep_files/find_and_count.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-string = 'hello'
-print(string.find('l'))  # will return first index
-print(string.find('f'))  # will return -1
-
-print(string.count('l'))  # will return 2
-print(string.count('f'))  # will return 0
