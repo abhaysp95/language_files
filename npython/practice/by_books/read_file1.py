c65e094fc8779ac034f1d410ddad92ec5c89de0c
coolabhays <abhaysp9955@gmail.com>
2020-07-04 21:45:20 +0530

    removed

--- npython/practice/by_books/read_file1.py
+++
@@ -1,8 +0,0 @@
-# this is the line to enter in file# this is the line to enter in file
-This is next line# this is second attempt# this is the line to enter in file
-This is next line
-# this is second attempt# this is the line to enter in file
-This is next line
-# this is second attempt'This is\ngoing to be\tthird line'# this is the line to enter in file
-This is next line
-# this is second attempt'This is\ngoing to be\tthird line'
--- npython/practice/by_books/string_ex5.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-str = 'X-DSPAM-Confidence:0.8475'
-
-got_str = float(str[str.find(':') + 1:])
-
-print('%g and type is %s' % (got_str, type(got_str)))
