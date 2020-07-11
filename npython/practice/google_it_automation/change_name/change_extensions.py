203720862f703fdd71e640a2899c79d5e2321b91
coolabhays <abhaysp9955@gmail.com>
2020-07-11 22:27:53 +0530

    added more functionality

--- npython/practice/google_it_automation/change_name/change_extensions.py
+++ npython/practice/google_it_automation/change_name/change_extensions.py
@@ -7,13 +7,16 @@ change all the given extension file to another extension
 import os
 import re
 import subprocess
+import sys
 
 
-def get_content(old_extension):
+def get_content(old_extension, path='.'):
     '''get the contents of dir'''
-    all_content = os.listdir('.')
+    sep = os.path.sep
+    all_content = os.listdir(path)
+    # print('get_content: ', path)
     files_list = [content for content in all_content
-                  if os.path.isfile(content)]
+                  if os.path.isfile(path + sep + content)]
     pattern = r"" + old_extension + "$"
     desired_files = [file for file in files_list
                      if re.search(pattern, file)]
@@ -25,12 +28,32 @@ def get_content(old_extension):
 
 def change_name(new_extension):
     '''change the extensions now'''
-    files = get_content(input('Enter old extension: '))
+    rel_path = '.'
+    sep = os.path.sep
+    try:
+        rel_path = sys.argv[1]
+        files = get_content(input('Enter old extension: '), rel_path)
+    except IndexError:
+        files = get_content(input('Enter old extension: '))
+
     for file in files:
         pattern = r"(.*)\..*$"
         file_new = re.sub(pattern, r"\1." + new_extension, file)
+        file = rel_path + sep + file
+        file_new = rel_path + sep + file_new
         subprocess.run(["mv", file, file_new], check=True)
     print('Renaming Successful')
 
 
-change_name(input('Enter new extension: '))
+def main():
+    '''main function'''
+    try:
+        if sys.argv[1] == "-h":
+            print("Usage: ./change_extensions [RELATIVE_PATH]")
+            sys.exit()
+    except IndexError:
+        change_name(input('Enter new extension: '))
+
+
+if __name__ == "__main__":
+    main()
