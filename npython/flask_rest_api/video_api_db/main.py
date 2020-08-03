06b6caa7124dd8e13fc428c084d7ca683382afb9
coolabhays <abhaysp9955@gmail.com>
2020-08-03 09:58:07 +0530

    finished REST basic

--- npython/flask_rest_api/video_api_db/main.py
+++ npython/flask_rest_api/video_api_db/main.py
@@ -1,8 +1,4 @@
-#!/usr/bin/env python3
-
-'''
-using rest api with database
-'''
+#!/usr/bin/env python3 ''' using rest api with database '''
 
 from flask import Flask
 from flask import request
@@ -28,15 +24,25 @@ class VideoModel(database.Model):
     likes = database.Column(database.Integer, nullable=False)
 
     def __repr__(self):
-        return f"Video(name = {name}, views = {views}, likes = {likes})"
+        return f"Video(id: {self.id}, name: {self.name}, views: {self.views}, likes: {self.likes})"
 
 
+# database.create_all()  # do this first time
+# or create database using interpretor
+
+# makes sure that request is passed and parsed
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of video is required", required=True)
 video_put_args.add_argument("views", type=int, help="Views of video is required", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes of video is required", required=True)
 
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name of video is required")
+video_update_args.add_argument("views", type=int, help="Views of video is required")
+video_update_args.add_argument("likes", type=int, help="Likes of video is required")
 
+
+# for json serialization
 resource_fields = {
     'id': fields.Integer,
     'name': fields.String,
@@ -51,13 +57,18 @@ class Video(Resource):
     # takes return and serializes using resource_fields
     @marshal_with(resource_fields)
     def get(self, video_id):
-        result = VideoModel.query.get(id=video_id)
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Video couln't be found with this id...")
         return result  # returns object of instance of VideoModel
 
     @marshal_with(resource_fields)
     def put(self, video_id):
         '''make a new object in database'''
         args = video_put_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="Video id already exist...")
         video = VideoModel(
             id=video_id,
             name = args['name'],
@@ -67,6 +78,35 @@ class Video(Resource):
         database.session.add(video)  # temporary adding
         database.session.commit()  # making permanent
         return video, 201
+
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        '''udpates for the video'''
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Can't Update, Video doesn't exist")
+        # elements are already in args, cause reqparse make them None if not given
+        # so, the values are not None
+        if args["name"]:
+            result.name = args["name"]
+        if args["views"]:
+            result.views = args["views"]
+        if args["likes"]:
+            result.likes = args["likes"]
+
+        # video already in db, so no need to add again, just commit
+        database.session.commit()
+        return result, 201
+
+    def delete(self, video_id):
+        '''delete the given video'''
+        result = VideoModel.query.filter_by(id=video_id)
+        if not result:
+            abort(404, message="Can't delete, Video doesn't exist")
+        result.delete()
+        database.session.commit()
+        return "Video Deleted", 204
 
 
 api.add_resource(Video, "/video/<int:video_id>")
--- npython/flask_rest_api/video_api_db/test.py
+++ npython/flask_rest_api/video_api_db/test.py
@@ -16,7 +16,7 @@ data = [
 
 for i in range(len(data)):
     response = requests.put(BASE + "/video/" + str(i), data[i])
-    # print({i: response.json()})
+    print({i: response.json()})
 print("Data inserted")
 
 input()
@@ -28,3 +28,36 @@ for i in range(len(data)):
 # to show the mesage if something isn't found
 response = requests.get(BASE + "/video/6")
 print(response.json())
+
+update_data = [
+    { "likes": 50, "name": "Myname3", "views": 10022 },
+    { "likes": 80, "name": "Myname4", "views": 10043 },
+    { "likes": 110, "name": "Myname5", "views": 10604 } ]
+
+input()
+for i in range(3):
+    j = 2 + i
+    response = requests.patch(BASE + "/video/" + str(j), update_data[i])
+    print({j: response.json()})
+# update individual
+response = requests.patch(BASE + "/video/0", {"likes": 2100, "view": 5500})
+print("Data Updated")
+
+input()
+print("get the videos with id ")
+for i in range(len(data)):
+    response = requests.get(BASE + "/video/" + str(i))
+    print(response.json())
+
+input()
+for i in range(2):
+    j = 3 + i
+    response = requests.delete(BASE + "/video/" + str(j))
+    print(response)
+print("All deletion done")
+
+input()
+print("get the videos with id ")
+for i in range(len(data)):
+    response = requests.get(BASE + "/video/" + str(i))
+    print(response.json())
