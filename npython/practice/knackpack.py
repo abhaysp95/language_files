0b29ec419fea57f340f5d712a7c29d9dcaa7eefd
coolabhays <abhaysp9955@gmail.com>
2020-08-12 18:35:48 +0530

    not completed yet

---
+++ npython/practice/knackpack.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+'''
+solve knackpack problem
+'''
+
+def get_all_sets(things, weights, max_weight):
+    '''get the set of things for all the weights'''
+
+
+
+
+things = [ "Apple", "Mango", "Pear", "Grapes"]
+weights = [ 2, 3, 1, 4 ]
+values = [ 4, 5, 3, 7 ]
+max_weight = 5
+get_all_sets(things, weights, values)
