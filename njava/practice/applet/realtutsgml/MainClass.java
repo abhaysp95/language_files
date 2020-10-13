25daf69fc3123d75edb270ff306d50dfa8964f3e
coolabhays <abhaysp9955@gmail.com>
2020-10-13 08:57:16 +0530

    Main Class for game

---
+++ njava/practice/applet/realtutsgml/MainClass.java
@@ -0,0 +1,56 @@
+import java.applet.*;
+import java.awt.*;
+
+public class MainClass extends Applet implements Runnable {
+	Thread thr = new Thread(this);
+	Player p;
+	Enemy e1, e2, e3, e4;
+	Image dbImage;
+	Graphics dbg;
+	private boolean running = true;
+	public void init() {
+		setBackground(Color.BLUE);
+		setSize(1000, 600);
+		this.p = new Player(this);
+		this.e1 = new Enemy(this);
+		this.e2 = new Enemy(600, 450, this);  // e1, e2 are having motions for x and y both, how ?
+		this.e3 = new Enemy(200, 450, this);
+		this.e4 = new Enemy(700, 450, this);
+	}
+	public void start() { this.thr.start(); }
+	public void stop() { this.running = false; }
+	public void destroy() { this.running = false; }
+	public void run() {
+		while (running) {
+			repaint();
+			this.p.update(this);
+			this.e1.update(this, "horizontal");
+			this.e2.update(this, "horizontal");
+			this.e3.update(this, "vertical");
+			this.e4.update(this, "vertical");
+			try {
+				Thread.sleep(20);
+			}
+			catch (InterruptedException ie) {
+				System.out.println("Some interruption error occured");
+			}
+		}
+	}
+	// update() is bydefault run by repaint() and update() by default runs paint()
+	@Override
+	public void update(Graphics g) {
+		this.dbImage = createImage(getWidth(), getHeight());
+		this.dbg = this.dbImage.getGraphics();
+		paint(dbg);
+		g.drawImage(this.dbImage, 0, 0, this);
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		this.p.paint(g, this);
+		this.e1.paint(g, this);
+		this.e2.paint(g, this);
+		this.e3.paint(g, this);
+		this.e4.paint(g, this);
+	}
+}
