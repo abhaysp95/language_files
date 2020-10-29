9ef4eae62a8da79299ce1671ecfb669150e3f5e6
coolabhays <abhaysp9955@gmail.com>
2020-10-29 07:05:43 +0530

    removed

--- njava/practice/udemy_tut/section9/inner_class/challenge/music_playlist/ITestInterfaceAlbum.java
+++
@@ -1,7 +0,0 @@
-package practice.udemy_tut.section9.inner_class.challenge.music_playlist;
-
-
-public interface ITestInterfaceAlbum {
-	boolean addSongInnerClass(Song song);
-	Song findSong(int trackNumber);
-}
