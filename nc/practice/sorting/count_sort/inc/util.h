9c0f09d0319aa37839222a7325ebe51534d40fdb
coolabhays <abhaysp9955@gmail.com>
2020-11-03 22:27:55 +0530

    new makefile

---
+++ nc/practice/sorting/count_sort/inc/util.h
@@ -0,0 +1,12 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#define SIZE 8
+
+int max(int *arr);
+int min(int *arr);
+
+int* sort(int *arr, int *newArr);
+void print(int *arr);
+
+#endif
---
+++ nc/practice/sorting/count_sort/src/util.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include "util.h"
+
+// get max number
+int max(int *arr) {
+	int max = *arr;
+	for (int i = 0; i < SIZE; ++i) {
+		if (max < *(arr + i)) {
+			max = *(arr + i);
+		}
+	}
+	return max;
+}
+
+// get min number
+int min(int *arr) {
+	int min = *arr;
+	for (int i = 0; i < SIZE; ++i) {
+		if (min > *(arr + i)) {
+			min = *(arr + i);
+		}
+	}
+	return min;
+}
+
+// actual count sort function
+/*******************************************************/
+
+int *sort(int *arr, int *newarr) {
+	int k = max(arr);
+	int temp[k + 1];
+
+	int i = 0;
+	// inclusive range
+	while (i <= k) {
+		*(temp + i++) = 0;
+	}
+
+	// store the count of each element
+	i = 0;
+	while (i < SIZE) {
+          // fill in the count of each element of arr to the position which is
+          // equal to the value of element of arr
+          /* like if 3 comes 2 times in arr, then temp[3] = 2 */
+          *(temp + *(arr + i++)) += 1;
+	}
+
+	// store the cummulative sum of the elements of temp array
+	i = 1;
+	while (i <= k) {
+		*(temp + i) += *(temp + (i - 1));
+		i++;
+	}
+
+	// find index of each element of orignal array in the temp array and place the elements in the newarr
+	i = SIZE - 1;
+	while (i >= 0) {
+		*(newarr + *(temp + *(arr + i)) - 1) = *(arr + i);
+		*(temp + *(arr + i)) -= 1;
+		i--;
+	}
+	return newarr;
+}
+
+/*******************************************************/
+
+
+void print(int *arr) {
+	for (int i = 0; i < SIZE; ++i) {
+		printf("%d\n", *(arr + i));
+	}
+}
