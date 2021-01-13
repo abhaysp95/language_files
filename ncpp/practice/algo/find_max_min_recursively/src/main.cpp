88ce74430e5bfd1602fa4d60764f2bd8762bed20
coolabhays <abhaysp9955@gmail.com>
2021-01-13 16:06:35 +0530

    added third approach with struct

--- ncpp/practice/algo/find_max_min_recursively/src/main.cpp
+++ ncpp/practice/algo/find_max_min_recursively/src/main.cpp
@@ -10,10 +10,18 @@
 #define ARRAY_SIZE 10
 #define MM 2
 
+struct Result {
+	int max{};
+	int min{};
+};
+
 std::array<int, MM> find_max_min(const std::array<int, ARRAY_SIZE>& array, size_t start,
 		size_t end, int& max, int& min);
 void find_max_min(const std::array<int, ARRAY_SIZE>& array,
 		std::array<int, MM>& resultant, size_t start, size_t end,
+		int& max, int& min);
+void find_max_min(const std::array<int, ARRAY_SIZE>& array,
+		Result& result, size_t start, size_t end,
 		int& max, int& min);
 
 int main(int argc, char **argv) {
@@ -23,11 +31,13 @@ int main(int argc, char **argv) {
 		std::cin >> numbers[i];
 	}
 	//std::array<int, ARRAY_SIZE> numbers = {2, 6, 8, 10, 9, 6, 5, 4, 3, 1};
+	Result result;
 	int max{}, min{};
 	//std::array<int, MM> resultant = find_max_min(numbers, 0, ARRAY_SIZE - 1, max, min);
-	std::array<int, MM> resultant;
-	find_max_min(numbers, resultant, 0, ARRAY_SIZE - 1, max, min);
-	std::cout << "Max: " << resultant[0] << ", Min: " << resultant[1] << std::endl;
+	//std::array<int, MM> resultant;
+	//find_max_min(numbers, resultant, 0, ARRAY_SIZE - 1, max, min);
+	find_max_min(numbers, result, 0, ARRAY_SIZE - 1, max, min);
+	std::cout << "Max: " << result.max << ", Min: " << result.min << std::endl;
 return 0;
 }
 
@@ -87,8 +97,38 @@ void find_max_min(const std::array<int, ARRAY_SIZE>& array,
 		// find max and min after both recursive calls are done for one time
 		max = compare_set1[0] > compare_set2[0] ? compare_set1[0] : compare_set2[0];
 		min = compare_set1[1] < compare_set2[1] ? compare_set1[1] : compare_set2[1];
-		//return_arr = {max, min};
 		resultant[0] = max;
 		resultant[1] = min;
+	}
+}
+
+
+/** third approach */
+
+void find_max_min(const std::array<int, ARRAY_SIZE>& array,
+		Result& result, size_t start, size_t end, int& max, int& min) {
+	if (start == end) {  // if there's only one element in the array left
+		max = min = array[start];
+		result.max = max;
+		result.min = min;
+	}  // first base condition
+	else if (start == end - 1) {  // if there are only two elements left
+		max = array[start] < array[end] ? array[end] : array[start];
+		min = array[start] > array[end] ? array[end] : array[start];
+		result.max = max;
+		result.min = min;
+	}  // second base condition
+	else {
+		size_t mid = (end + start) / 2;
+		// first recursive call will be from start(0) to mid value
+		std::array<int, MM> compare_set1 = find_max_min(array, start, mid, max, min);
+		// second recursive call will be from mid + 1 to end
+		std::array<int, MM> compare_set2 = find_max_min(array, mid + 1, end, max, min);
+		// in both compare set, first index is going to be maximum and second is minimum
+		// find max and min after both recursive calls are done for one time
+		max = compare_set1[0] > compare_set2[0] ? compare_set1[0] : compare_set2[0];
+		min = compare_set1[1] < compare_set2[1] ? compare_set1[1] : compare_set2[1];
+		result.max = max;
+		result.min = min;
 	}
 }
