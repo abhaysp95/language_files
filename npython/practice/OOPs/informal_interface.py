b67eb86bbb1bc500e5b2ca0499238938ef87b7de
coolabhays <abhaysp9955@gmail.com>
2021-03-21 21:33:48 +0530

    few things on virtual base class

--- npython/practice/OOPs/informal_interface.py
+++ npython/practice/OOPs/informal_interface.py
@@ -101,5 +101,51 @@ print("------------------------------\n")
 
 # Using Virtual Base Classes
 
+class PersonMeta(type):
+    """A person metaclasss"""
+    def __instancecheck__(cls, instance):
+        return cls.__subclasscheck__(type(instance))
+
+    def __subclasscheck__(cls, subclass):
+        return (hasattr(subclass, "name")
+                and callable(subclass.name)
+                and hasattr(subclass, "age")
+                and callable(subclass.age))
+
+class PersonSuper:
+    """A person superclass"""
+    def name(self) -> str:
+        pass
+
+    def age(self) -> int:
+        pass
+
+class Person(metaclass=PersonMeta):
+    """Person interface built from PersonMeta metaclass"""
+
+# Here, you have the setup for creating virtual base classes:
+# 1. The metaclass PersonMeta
+# 2. The base class PersonSuper
+# 3. The python interface Person
+
+# Employee class inherits PersonSuper
+# Inheriting Subclass
+class Employee(PersonSuper):
+    """Inherits from PersonSuper
+    PersonSuper will appear in Employee.__mro__"""
+    pass
+
+class Friend:
+    """Built implicitly from Person
+    Friend is a virtual subclass of Person since
+    both required methods exist.
+    Person not in Friend.__mro__"""
+    def name(self):
+        pass
+
+    def age(self):
+        pass
+# due to implementations of .name() and .age(), Person becomes a virtual base class
+# of Friend
 
 # reference: https://realpython.com/python-interface/#formal-interfaces
