97283e505c969f3a676d55c6c87b235abc576cc1
coolabhays <abhaysp9955@gmail.com>
2021-03-29 14:05:55 +0530

    fixed insertion sort and applied quicksort

--- ncpp/practice/data_structure/arrays/pair_sum_problem/src/main.cpp
+++ ncpp/practice/data_structure/arrays/pair_sum_problem/src/main.cpp
@@ -2,11 +2,15 @@
 
 #include <iostream>
 #include <cassert>
+#include <cmath>
 
 bool brute_force_way(const int* arr, const size_t& arr_size, const int& key_sum);
 bool better_way(const int* arr, const size_t& arr_size, const int& key_sum);
 void sort(int* arr, const size_t& arr_size);  // sort in non-decreasing order
 void insertion_sort(int* arr, const size_t& arr_size);
+int median(const int* arr, const size_t& begin, const size_t& end);
+void quicksort(int* arr, size_t begin, size_t end);
+size_t partition(int* arr, size_t begin, size_t end);
 
 int main(int argc, char **argv) {
 	int key_sum{};
@@ -22,14 +26,19 @@ int main(int argc, char **argv) {
 		std::cin >> *(arr + idx);
 	}
 
-	/*if (brute_force_way(arr, arr_size, key_sum)) {
-		std::cout << "Pair found";
-	}
-	else {
-		std::cout << "Pair not found";
-	}*/
-
+	// sorting is not necessary for brute force way
 	sort(arr, arr_size);
+
+	printf("\nAfter sorting: \n[ ");
+	for (size_t idx = 0; idx < arr_size; ++idx) {
+		printf("%d", *(arr + idx));
+		if (idx < arr_size - 1) {
+			printf(", ");
+		}
+	}
+	printf(" ]\n");
+
+	// you can try brute_force_way() here too
 	if (better_way(arr, arr_size, key_sum)) {
 		std::cout << "Pair found";
 	}
@@ -40,30 +49,96 @@ int main(int argc, char **argv) {
 	return 0;
 }
 
+int median(const int* arr, const size_t& begin, const size_t& end) {
+	size_t mid = (begin + end) / 2;
+	if (*(arr + begin) < *(arr + mid)) {
+		if (*(arr + begin) >= *(arr + end)) {
+			return *(arr + begin);
+		}
+		else if (*(arr + mid) < *(arr + end)) {
+			return *(arr + mid);
+		}
+	}
+	else {
+		if (*(arr + begin) < *(arr + end)) {
+			return *(arr + begin);
+		}
+		else if (*(arr + mid) >= *(arr + end)) {
+			return *(arr + mid);
+		}
+	}
+	return *(arr + end);
+}
+
 void sort(int* arr, const size_t& arr_size) {
-	if (arr_size < 20) {
+	if (arr_size < 10) {  // this is the count which js uses to switch probably
 		// insertion sort is better for array with small size
 		insertion_sort(arr, arr_size);
 	}
 	else {
-		// do quick sort
+		// quicksort good with large set of elements
+		quicksort(arr, 0, arr_size - 1);
 	}
 }
 
 void insertion_sort(int* arr, const size_t& arr_size) {
 	for (size_t idx = 1; idx < arr_size; ++idx) {
-		int key = *(arr + idx);
-		size_t sidx = idx - 1;
-		while (sidx > 0 && *(arr + sidx) > key) {
-			*(arr + sidx + 1) = *(arr + sidx);
+		size_t sidx = idx;
+		while (sidx > 0 && *(arr + sidx) < *(arr + sidx - 1)) {
+			int temp = *(arr + sidx);
+			*(arr + sidx) = *(arr + sidx - 1);
+			*(arr + sidx - 1) = temp;
 			sidx--;
 		}
-		*(arr + sidx) = key;
 	}
 }
 
-size_t get_pivot(const int* arr, const size_t& arr_size);
-int* do_partition(int* arr, const size_t& arr_size);
+void quicksort(int* arr, size_t begin, size_t end) {
+	size_t pivot_index{};
+
+	if (begin != end) {
+		pivot_index = partition(arr, begin, end);
+	}
+
+	// if begin reference is not incremented upto pivot element yet, keep comparing
+	if (begin < pivot_index - 1) {
+		quicksort(arr, begin, pivot_index - 1);
+	}
+
+	// if end reference is not upto pivot_index yet, keep comparing
+	if (pivot_index < end) {
+		quicksort(arr, pivot_index, end);
+	}
+}
+
+size_t partition(int* arr, size_t begin, size_t end) {
+	int pivot_element{};
+	size_t left = begin;
+	size_t right = end;
+
+	if ((end - begin) > 2) {  // contains atleast 3 elements
+		pivot_element = median(arr, begin, end);
+	}
+	else {
+		pivot_element = *(arr + (int)floor((begin + end) / 2));
+	}
+
+	while (left <= right) {
+		while (*(arr + left) < pivot_element) { left++; }
+		while (*(arr + right) > pivot_element) { right--; }
+
+		// swap left and right index elements if left & right haven't crossed each-other
+		if (left <= right) {
+			int temp = *(arr + left);
+			*(arr + left) = *(arr + right);
+			*(arr + right) = temp;
+
+			left++;
+			right--;
+		}
+	}
+	return left;
+}
 
 // time complexity: O(n^2) worst case
 bool brute_force_way(const int* arr, const size_t& arr_size, const int& key_sum) {
