25eb07f430020b716d6106b1ea97cbedcdb43002
coolabhays <abhaysp9955@gmail.com>
2021-07-25 14:56:49 +0530

    testing build from book

---
+++ ncpp/modern_cmake/test1/apps/calc.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include <calc/lib.hpp>
+
+int main(int argc, char** argv) {
+	std::cout << "From main: \n";
+	std::cout << func_lib();
+	std::cout << std::endl;
+}
---
+++ ncpp/modern_cmake/test1/include/calc/lib.hpp
@@ -0,0 +1,8 @@
+#ifndef CALC_LIB_HPP
+#define CALC_LIB_HPP
+
+#include <string>
+
+std::string func_lib();
+
+#endif
