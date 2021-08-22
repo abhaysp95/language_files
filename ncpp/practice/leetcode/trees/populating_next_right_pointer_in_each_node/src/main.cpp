3a807aa349b6b720681229519fdb658eee5bab8c
coolabhays <abhaysp9955@gmail.com>
2021-08-22 20:12:07 +0530

    more trees question

---
+++ ncpp/practice/leetcode/trees/populating_next_right_pointer_in_each_node/src/main.cpp
@@ -0,0 +1,227 @@
+//main file
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define lD long double
+#define PI 3.1415926535897932384626
+#define INF 1e9
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repl(i, n) for (ll i = 0; i < (ll)(n); i++)
+#define repd(i, n) for (int i = (n) - 1; i >= 0; i--)
+#define repld(i, n) for (ll i = (n) - 1; i >= 0; i--)
+#define loop(i,a,b) for (int i = (int)(a); i < (int)(b); i++)
+#define loops(i,a,b,s) for (int i = (int)(a); i < (int)(b); i+=(s))
+#define rloop(i,a,b) for (int i = (int)(a); i <= (int)(b); i++)
+#define rloops(i,a,b,s) for (int i = (int)(a); i <= (int)(b); i+=(s))
+#define loopl(i,a,b) for (ll i = (ll)(a); i < (ll)(b); i++)
+#define loopls(i,a,b,s) for (ll i = (ll)(a); i < (ll)(b); i+=(s))
+#define rloopl(i,a,b) for (ll i = (ll)(a); i <= (ll)(b); i++)
+#define rloopls(i,a,b,s) for (ll i = (ll)(a); i <= (ll)(b); i+=(s))
+#define loopd(i,a,b) for (int i = (int)(a); i > (b); i--)
+#define loopds(i,a,b,s) for (int i = (int)(a); i > (b); i-=(s))
+#define rloopd(i,a,b) for (int i = (int)(a); i >= (b); i--)
+#define rloopds(i,a,b,s) for (int i = (int)(a); i >= (b); i-=(s))
+#define loopld(i,a,b) for (ll i = (ll)(a); i > (b); i--)
+#define looplds(i,a,b,s) for (ll i = (ll)(a); i > (b); i-=(s))
+#define rloopld(i,a,b) for (ll i = (ll)(a); i >= (b); i--)
+#define rlooplds(i,a,b,s) for (ll i = (ll)(a); i >= (b); i-=(s))
+#define wl (t) while((t)--)
+#define pb push_back
+#define eb emplace_back
+#define ab(a) (a < 0)?(-1*(a)):(a)
+#define mset(a,b,c) loop(i,0,(b)) (a)[i]=(c)
+#define asum(a,b,c) { int i = 0; (c) = 0; repd(i, b) (c)+=(a)[i]; }
+#define fe first
+#define se second
+#define mp make_pair
+#define clr(x) (x).clear()
+#define itoc(c) ((char)(((int)'0')+(c)))
+#define all(p) (p).begin(),(p).end()
+#define rall(p) (p).rbegin(),(p).rend()
+#define tr(c,it) for(__typeof__((c).begin()) it = (c).begin(); it != (c).end(); ++it)
+#define trr(c,it) for(__typeof__((c).rbegin()) it = (c).rbegin(); it != (c).rend(); ++it)
+#define present(c,x) ((c).find(x) != (c).end())  // container version, log(n) for set and map
+#define cpresent(c,x) (find(all(c),(x)) != (c).end())  // works for all container
+#define mx(x,y) ((x)>(y)?(x):(y))
+#define mn(x,y) ((x)<(y)?(x):(y))
+#define rmx(a,b) ((a) = mx((a),(b)))
+#define rmn(a,b) ((a) = mn((a),(b)))
+#define mid(s,e) ((s)+((e)-(s))/2)
+#define mod(a,b) ((a)-((a)/(b))*(b))
+#define pq(type) priority_queue<type>
+#define pqd(type) priority_queue<type,vector<type>,greater<type>>
+#define umap unordered_map
+#define uset unordered_set
+#define sz(x) ((int)(x).size())
+#define inrange(i,a,b) (((i)>=mn(a,b)) && ((i)<=mx(a,b)))
+#define cfp(x) 	cout << fixed << showpoint; 	cout << setprecision(x);
+#define nl "\n"
+#define br cout << "\n"
+
+#define fileI(name) \
+	freopen(name".in", "r", stdin)
+#define fileIO(name) { \
+	freopen(name".in", "r", stdin); \
+	freopen(name".out", "w", stdout); \
+}
+#define FAST_IO { \
+	ios_base::sync_with_stdio(false); \
+	cin.tie(NULL); \
+	cout.tie(NULL); \
+}
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wcomment"
+
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vs = vector<string>;
+using vvs = vector<vs>;
+using vd = vector<double>;
+using vvd = vector<vd>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vpll = vector<pll>;
+using vpii = vector<pii>;
+
+mt19937_64 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+int rngi(int lim) {
+	uniform_int_distribution<int> uid(0,(lim)-1);
+	return uid(rang);
+}
+double rngr(double lim) {
+	uniform_real_distribution<double> urd(0,(lim)-1);
+	return urd(rang);
+}
+
+template<typename T> T gcd(T a, T b) { return ((b)?__gcd(a,b):(a)); }
+template<typename T> T lcm(T a, T b) { return ((a)*((b)/gcd(a,b))); }
+
+inline void tokenize(const string& str, vs& out, const char&& delim) {
+	stringstream ss(str);
+	string s;
+	while (getline(ss, s, delim)) out.pb(s);
+}
+inline void tokenize(const string& str, vd& out, const char&& delim) {
+	stringstream ss(str);
+	for (double i; ss >> i;) { out.pb(i); if (ss.peek() == delim) ss.ignore(); }
+}
+inline void tokenize(const string& str, vi& out, const char&& delim) {
+	stringstream ss(str);
+	for (int i; ss >> i;) { out.pb(i); if (ss.peek() == delim) ss.ignore(); }
+}
+inline void print_mat(const vvi& t) {
+	if(0 == t.size()) {
+		cout << "[WW] Empty Matrix!!!" << nl;
+		return;
+	}
+	rep(i, t.size()) {
+		rep(j, t[0].size()) cout << t[i][j] << " ";
+		br;
+	}
+}
+inline void ltrim(string& s) { s.erase(s.begin(), find_if(all(s), not1(ptr_fun<int, int>(isspace)))); }
+inline void rtrim(string& s) { s.erase(find_if(rall(s), not1(ptr_fun<int, int>(isspace))).base(), s.end()); }
+inline void trim(string& s) { ltrim(s); rtrim(s); }
+inline string cltrim(string s) { ltrim(s); return s; }
+inline string crtrim(string s) { rtrim(s); return s; }
+
+void solvethetestcase();
+
+signed main() {
+	//comment when using scanf, printf
+	FAST_IO;
+
+	//set the seed
+	//srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+	//comment for input through console
+	fileI("input");
+
+	int t = 1;
+	//(uncomment for multiple test cases)
+	//cin >> t;
+	//cin.ignore();
+	loopl (testcase, 1, t + 1) {
+		//(uncomment for multiple test cases)
+		//cout << "Case #" << testcase << ": ";
+		//br;
+		solvethetestcase();
+	}
+}
+
+typedef struct _node {
+	int val;
+	_node *left;
+	_node *right;
+	_node *next;
+	_node(): val{}, left{nullptr}, right{nullptr}, next{nullptr} {}
+	_node(int v): val{v}, left{nullptr}, right{nullptr}, next{nullptr} {}
+	_node(int v, _node *l, _node *r, _node *n): val{v}, left{l}, right{r}, next{n} {}
+} Node;
+
+Node* connect(Node *root) {
+	if(!root) return root;
+	Node *ptr{root};
+	queue<Node *> q{};
+	q.push(root);  // next of root is already pointing to nullptr(constructor)
+	q.push(nullptr);
+	while(!q.empty()) {
+		ptr = q.front();
+		q.pop();
+		if(ptr) {
+			ptr->next = q.front();  // just next element of ptr(before popping ptr)
+			if(ptr->left) q.push(ptr->left);
+			if(ptr->right) q.push(ptr->right);
+		}
+		else {
+			if(!q.empty()) q.push(nullptr);
+		}
+	}
+	return root;
+}
+
+/** can't merge this functionality in above function(because above function is
+ * the answer for question) */
+vector<Node *> level_order_traversal(Node *root) {
+	/** this function is designed just for this question only
+	  * this function makes sure that above function 'connect()' has done its
+	  * job correctly
+	  */
+	vector<Node *> res{};
+	if(!root) return res;
+	Node *ptr{root};
+	queue<Node *> q{};
+	while(ptr != nullptr) {
+		Node *pptr{ptr};
+		while(pptr != nullptr) {
+			res.push_back(pptr);
+			pptr = pptr->next;
+		}
+		res.push_back(nullptr);
+		ptr = ptr->left;
+	}
+	return res;
+}
+
+void solvethetestcase() {
+	Node *root = new Node(1);
+	root->left = new Node(2);
+	root->right = new Node(3);
+	root->left->left = new Node(4);
+	root->left->right = new Node(5);
+	root->right->left = new Node(6);
+	root->right->right = new Node(7);
+	root = connect(root);
+	vector<Node *> res = level_order_traversal(root);
+	for_each(res.begin(), res.end(), [](const Node* node) {
+			if(!node) cout << "#, ";
+			else cout << node->val << ", ";
+			});
+	br;
+}
+
+#pragma GCC diagnostic pop
