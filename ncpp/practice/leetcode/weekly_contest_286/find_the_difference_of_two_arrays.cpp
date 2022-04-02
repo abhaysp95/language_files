f7489d8cccf925ee3a016082bb9496477b031530
coolabhays <abhaysp9955@gmail.com>
2022-04-02 19:48:31 +0530

    fix: update with correct solution

--- ncpp/practice/leetcode/weekly_contest_286/find_the_difference_of_two_arrays.cpp
+++ ncpp/practice/leetcode/weekly_contest_286/find_the_difference_of_two_arrays.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 
 
 using namespace std;
+
+class Solution
+{
+	public:
+		vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2)
+		{
+			vector<vector<int>> ret(2);
+			unordered_set<int> s1 {nums1.begin(), nums1.end()},
+				s2 {nums2.begin(), nums2.end()};
+
+			for (const auto& x: s1)
+				if (!s2.count(x))
+					ret[0].push_back(x);
+
+			for (const auto& x: s2)
+				if (!s1.count(x))
+					ret[1].push_back(x);
+
+			return ret;
+		}
+};
 
 vector<int> difference(const vector<int>& num1, const vector<int>& num2)
 {
@@ -52,7 +74,9 @@ vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2)
 int main(int argc, char** argv)
 {
 	vector<int> a{1, 2, 3, 3}, b{2, 4, 6};
-	auto res {findDifference(a, b)};
+	// auto res {findDifference(a, b)};
+	Solution sol {};
+	auto res {sol.findDifference(a, b)};
 	for (const auto& vec: res)
 	{
 		for (const auto x: vec)
