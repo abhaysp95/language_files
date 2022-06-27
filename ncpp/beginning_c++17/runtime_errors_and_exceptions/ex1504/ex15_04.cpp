1137e410786173835c0dcd399beb463a35bb0936
coolabhays <abhaysp9955@gmail.com>
2022-06-27 12:47:48 +0530

    feat: matching catch handler to exception

---
+++ ncpp/beginning_c++17/runtime_errors_and_exceptions/ex1504/ex15_04.cpp
@@ -0,0 +1,31 @@
+#include "mytroubles.hpp"
+#include <iostream>
+
+void trySomething(int i);
+
+int main(void) {
+	Trouble trouble;
+	MoreTrouble more_trouble;
+	BigTrouble big_trouble;
+
+	for (size_t i {}; i < 7; i++) {
+		try {
+			if (i == 3) {
+				throw trouble;
+			} else if (i == 5) {
+				throw more_trouble;
+			} else if (i == 6) {
+				throw big_trouble;
+			}
+		} catch (const BigTrouble& t) {
+			std::cout << "BigTrouble object caught: " << t.what() << std::endl;
+		} catch (const MoreTrouble& t) {
+			std::cout << "MoreTrouble object caught: " << t.what() << std::endl;
+		} catch (const Trouble& t) {
+			std::cout << "Trouble object caught: " << t.what() << std::endl;
+		}
+		std::cout << "End of for loop (after the catch blocks) - i is " << i << std::endl;
+	}
+
+	return 0;
+}
