147056d374181407973d2c3eb22ee9063ccffce4
abhay <abhaysp95@protonmail.com>
2023-03-14 11:17:59 +0530

    feat: about trait extension

--- nrust/programming_rust/generics_traits/src/main.rs
+++ nrust/programming_rust/generics_traits/src/main.rs
@@ -1,4 +1,10 @@
-use std::{fmt::Debug, fs::File, hash::Hash, io::Write, ops::Range};
+use std::{
+    fmt::Debug,
+    fs::File,
+    hash::Hash,
+    io::{Result, Write, self},
+    ops::Range, rc::Rc,
+};
 
 #[allow(dead_code)]
 // just "dyn Write" will not work, a reference is needed because of size issue
@@ -74,7 +80,7 @@ impl Canvas {
 impl Broom {
     /// helper function used by Broom::draw()
     fn broomstick_range(&self) -> Range<i32> {
-        self.y - self.height - 1 .. self.y
+        self.y - self.height - 1..self.y
     }
 }
 
@@ -104,7 +110,7 @@ impl Visible for Broom {
 
 /* default methods */
 /// A writer that ignores whatever data you write
-struct Sink { }
+struct Sink {}
 
 impl Write for Sink {
     fn write(&mut self, buf: &[u8]) -> Result<usize> {
@@ -118,6 +124,38 @@ impl Write for Sink {
     }
 }
 
+// extension trait
+trait IsEmoji {
+    fn is_emoji(&self) -> bool;
+}
+
+// could be any other type too
+impl IsEmoji for char {
+    fn is_emoji(&self) -> bool {
+        // add the logic here ...
+        return false;
+    }
+}
+
+/// a plain simple html document structure
+#[allow(dead_code)]
+struct HTMLDocument {
+    node_data: String,
+    childrens: Vec<Rc<HTMLDocument>>
+}
+
+/// trait values to which you can send HTML
+trait WriteHTML {
+    fn write_html(&mut self, html: &HTMLDocument) -> io::Result<()>;
+}
+
+impl<W: Write> WriteHTML for W {
+    fn write_html(&mut self, _html: &HTMLDocument) -> io::Result<()> {
+        // ... write html document to writer
+        Ok(())
+    }
+}
+
 fn main() {
     let mut local_file = File::create("hello.txt").unwrap();
     say_hello(&mut local_file).unwrap();
@@ -127,6 +165,8 @@ fn main() {
     say_hello(&mut bytes).unwrap();
     assert_eq!(bytes, b"hello world\n");
 
-    let mut out = Sink{};
-    out.write_all(b"Hello World\n").unwrap();  // where's write_all() defined ?
+    let mut out = Sink {};
+    out.write_all(b"Hello World\n").unwrap(); // where's write_all() defined ?
+
+    assert_eq!('$'.is_emoji(), false);
 }
