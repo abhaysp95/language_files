979474829daeafef9d8231e300d0d24e989043dd
abhay <abhaysp95@protonmail.com>
2023-04-02 13:43:20 +0530

    feat: using channels (multiple transmitter, single receiver)

--- nrust/the_rust_programming_language/fearless_concurrency/src/main.rs
+++ nrust/the_rust_programming_language/fearless_concurrency/src/main.rs
@@ -1,17 +1,43 @@
-use std::{thread, time::Duration};
+use std::{sync::mpsc, thread, time::Duration};
+
 
 fn main() {
-    let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {} from the spawned thread", i);
-            thread::sleep(Duration::from_millis(1));
+    let (tx, rx) = mpsc::channel();
+
+    let tx1 = tx.clone();
+    thread::spawn(move || {
+        let vals = vec![
+            "1. hi".to_string(),
+            "1. from".to_string(),
+            "1. the".to_string(),
+            "1. spawned".to_string(),
+            "1. thread".to_string(),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_millis(100));
+        }
+
+        // println!("After sending vals: {:?}", vals);  // can't do it, vals is already moved
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            "2. hi".to_string(),
+            "2. from".to_string(),
+            "2. the".to_string(),
+            "2. spawned".to_string(),
+            "2. thread".to_string(),
+        ];
+
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_millis(100));
         }
     });
 
-    for i in 1..5 {
-        println!("hi number {} from the main thread", i);
-        thread::sleep(Duration::from_millis(1));
+    for msg in rx {
+        println!("received: {msg}");
     }
-
-    handle.join().unwrap();
 }
